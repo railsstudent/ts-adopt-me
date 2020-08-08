a22263fdb31f0ba4cde7cbf090240895abbd894e
Connie Leung <cleung334@gmail.com>
2020-08-09 00:46:21 +0800

    refactor: Details component

--- src/Details.tsx
+++ src/Details.tsx
@@ -64,33 +64,35 @@ const Details = (props: { id: string }) => {
     const toggleModal = () => setShowModal(!showModal);
     const adopt = () => navigate(url);
 
-    if (!details) {
-        navigate('/');
-        return
-    }
+    // if (!details) {
+    //     navigate('/');
+    //     return
+    // }
 
     return (
         loading ? <h1>Loading...</h1> :
-            <div className="details">
-                <Carousel media={details.media} />
-                <div>
-                    <h1>{details.name}</h1>
-                    <h2>{`${details.animal} - ${details.breed} - ${details.location}`}</h2>
-                    <button style={{ backgroundColor, color }} onClick={toggleModal}>Adopt {details.name}</button>
-                    <p>{details.description}</p>
-                    {
-                        showModal ?
-                        <Modal>
-                            <h1>Would you like to adopt {details.name}?</h1>
-                            <div className="buttons">
-                                <button onClick={adopt}>Yes</button>
-                                <button onClick={toggleModal}>No</button>
-                            </div>
-                        </Modal>                        
-                        : null
-                    }
-                </div>
-            </div>
+            (details ? 
+                <div className="details">
+                    <Carousel media={details.media} />
+                    <div>
+                        <h1>{details.name}</h1>
+                        <h2>{`${details.animal} - ${details.breed} - ${details.location}`}</h2>
+                        <button style={{ backgroundColor, color }} onClick={toggleModal}>Adopt {details.name}</button>
+                        <p>{details.description}</p>
+                        {
+                            showModal ?
+                            <Modal>
+                                <h1>Would you like to adopt {details.name}?</h1>
+                                <div className="buttons">
+                                    <button onClick={adopt}>Yes</button>
+                                    <button onClick={toggleModal}>No</button>
+                                </div>
+                            </Modal>                        
+                            : null
+                        }
+                    </div>
+                </div> : null
+            )
     )
 }
 
